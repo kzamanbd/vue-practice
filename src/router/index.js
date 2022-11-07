7cd6edd212cca37fb18637a3226cb59aa8bfb3f2
Kamruzzaman <kzamanbn@gmail.com>
2022-11-08 00:28:35 +0600

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,77 +1,76 @@
 import { createRouter, createWebHistory } from "vue-router";
 import store from "@/store";
 
-const routes = [
-    {
-        path: "/login",
-        name: "Login",
-        component: () => import("@/pages/UserLogin.vue"),
-        meta: {
-            requiresAuth: false,
-            layout: "blank",
-        },
-    },
-
-    {
-        path: "/",
-        name: "Home",
-        component: () => import("@/pages/HomePage.vue"),
-        meta: {
-            requiresAuth: false,
-            layout: "default",
-        },
-    },
-
-    {
-        path: "/profile",
-        name: "Profile",
-        component: () => import("@/pages/UserProfile.vue"),
-        meta: {
-            requiresAuth: true,
-            layout: "default",
-        },
-    },
-
-    {
-        path: "/image-crop",
-        name: "Crop Image",
-        component: () => import("@/pages/ImageCrop.vue"),
-        meta: {
-            requiresAuth: false,
-            layout: "blank",
-        },
-    },
-
-    {
-        path: "/select-checkbox",
-        name: "Select Checkbox",
-        component: () => import("@/pages/SelectCheckbox.vue"),
-        meta: {
-            requiresAuth: false,
-            layout: "blank",
-        },
-    },
-
-    {
-        path: "/flatpickr",
-        name: "FlatPicker",
-        component: () => import("@/pages/FlatPicker.vue"),
-        meta: {
-            requiresAuth: false,
-            layout: "blank",
-        },
-    },
-
-    {
-        path: "/:pathMatch(.*)*",
-        name: "Not found",
-        component: () => import("@/pages/NotFound.vue"),
-    },
-];
 
 const router = createRouter({
     history: createWebHistory(),
-    routes,
+    routes: [
+        {
+            path: "/login",
+            name: "Login",
+            component: () => import("@/pages/UserLogin.vue"),
+            meta: {
+                requiresAuth: false,
+                layout: "blank",
+            },
+        },
+
+        {
+            path: "/",
+            name: "Home",
+            component: () => import("@/pages/HomePage.vue"),
+            meta: {
+                requiresAuth: false,
+                layout: "default",
+            },
+        },
+
+        {
+            path: "/profile",
+            name: "Profile",
+            component: () => import("@/pages/UserProfile.vue"),
+            meta: {
+                requiresAuth: true,
+                layout: "default",
+            },
+        },
+
+        {
+            path: "/image-crop",
+            name: "Crop Image",
+            component: () => import("@/pages/ImageCrop.vue"),
+            meta: {
+                requiresAuth: false,
+                layout: "blank",
+            },
+        },
+
+        {
+            path: "/select-checkbox",
+            name: "Select Checkbox",
+            component: () => import("@/pages/SelectCheckbox.vue"),
+            meta: {
+                requiresAuth: false,
+                layout: "blank",
+            },
+        },
+
+        {
+            path: "/flatpickr",
+            name: "FlatPicker",
+            component: () => import("@/pages/FlatPicker.vue"),
+            meta: {
+                requiresAuth: false,
+                layout: "blank",
+            },
+        },
+
+        {
+            path: "/:pathMatch(.*)*",
+            name: "Not found",
+            component: () => import("@/pages/NotFound.vue"),
+        },
+    ],
 });
 
 router.beforeEach((to, from, next) => {
