3bf6bcf573adc659ede1e4e36ec29e61eca94593
Kamruzzaman <kzamanbn@gmail.com>
2022-11-08 09:43:55 +0600

    Update IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -24,9 +24,9 @@
                     <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
                     <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
                     <li>
-                        <a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener"
-                            >vue-devtools</a
-                        >
+                        <a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">
+                            vue-devtools
+                        </a>
                     </li>
                     <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
                     <li>
