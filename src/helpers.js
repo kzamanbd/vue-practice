d9e61860a1145b960aeda5667650ccc1daaca6c5
Kamruzzaman <kzamanbn@gmail.com>
2022-11-20 12:53:36 +0600

    fixed tab indent error

--- src/helpers.js
+++ src/helpers.js
@@ -2,10 +2,10 @@
 
 import TopHeader from '@/components/TopHeader.vue';
 // axios default configuration
-import axios from "./plugins/axios";
+import axios from './plugins/axios';
 export default {
     install(app) {
         app.config.globalProperties.$axios = axios;
         app.component('TopHeader', TopHeader);
-    }
+    },
 };
--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import helpers from "./helpers";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import helpers from './helpers';
 
-import "bootstrap";
+import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const app = createApp(App)
-    .use(store)
-    .use(router)
-    .use(helpers);
-app.mount("#app");
+const app = createApp(App).use(store).use(router).use(helpers);
+app.mount('#app');
