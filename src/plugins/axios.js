2373c43065aa6deda72f15d30d5ed663b97a9e7b
Kamruzzaman <kzamanbn@gmail.com>
2022-12-16 01:00:45 +0600

    added static json report to api based report

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@
  * @Author: Kamruzzaman
  * @Date: 2022-11-02 00:19:45
  * @Last Modified by: Kamruzzaman
- * @Last Modified time: 2022-11-22 23:20:48
+ * @Last Modified time: 2022-12-16 00:47:44
  */
 // plugins/axios.js
 import axios from 'axios';
@@ -11,7 +11,7 @@ const httpRequest = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         Accept: 'application/json',
-        'Content-Type': 'application/json',
+        ContentType: 'application/json',
     },
 });
 
--- src/views/TestReport/TestReport.vue
+++ src/views/TestReport/TestReport.vue
@@ -1,15 +1,45 @@
 <template>
     <div style="display: flex; justify-content: center; align-items: center; height: 100vh">
-        <button type="button" class="btn btn-primary" @click="printReport">Print Report</button>
+        <button
+            type="button"
+            class="btn btn-primary"
+            @click="printReport"
+            :disabled="loading"
+            :class="{ disabled: loading }">
+            Print Report
+        </button>
     </div>
 </template>
 <script setup>
-    import report from '@/static/report.json';
+    import { ref } from 'vue';
     import template from './template';
+    import token from './token';
 
-    const printReport = () => {
-        console.log('print report', report);
-        const html = template(report.data);
+    const loading = ref(false);
+    const reportData = ref(null);
+
+    const printReport = async () => {
+        loading.value = true;
+        const response = await fetch('http://api.jerpbd.com:8885/api/web/get-sales-evaluation-sdr-062i', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                depot_id: '218',
+                area_id: 0,
+                mio_spo: 0,
+                target_type: 0,
+                disc_exclude: 1,
+                date_from: '15-12-2022',
+                date_to: '16-12-2022',
+            }),
+        });
+        reportData.value = await response.json();
+        loading.value = false;
+        console.log('reportData', reportData.value.data);
+        const html = template(reportData.value.data);
         const myWindow = window.open('', '_blank');
         myWindow.document.write(html);
         myWindow.document.close();
