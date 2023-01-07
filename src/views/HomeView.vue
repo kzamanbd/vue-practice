1177f7e6c9572e7f7785636eee13bb7112efec11
Kamruzzaman <kzamanbn@gmail.com>
2023-01-07 21:58:39 +0600

    update home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,22 +26,85 @@
         </main>
         <ContextMenu ref="menu">
             <template v-slot="{ contextData }">
-                <ContextMenuItem @click="closeMenu"> <a href="/blog" target="_blank">Blog</a> </ContextMenuItem>
-                <ContextMenuItem @click="closeMenu">
-                    <a href="/flatpickr" target="_blank">Date Time</a>
-                </ContextMenuItem>
-                <ContextMenuItem @click="closeMenu">
-                    <a href="/select-checkbox" target="_blank">Select Checkbox</a>
-                </ContextMenuItem>
-                <ContextMenuItem @click="closeMenu">
-                    <a href="/test-report" class="border-0" target="_blank">Test Report</a>
-                </ContextMenuItem>
-                <ContextMenuItem @click="closeMenu">
-                    <a href="/swiper-slider" class="border-0" target="_blank">Swiper Slider</a>
-                </ContextMenuItem>
+                <ContextMenuItem @click="$router.push('/blog')"> Blog </ContextMenuItem>
+                <ContextMenuItem @click="$router.push('/flatpickr')"> Date Time </ContextMenuItem>
+                <ContextMenuItem @click="$router.push('/select-checkbox')"> Select Checkbox </ContextMenuItem>
+                <ContextMenuItem @click="$router.push('/test-report')"> Test Report </ContextMenuItem>
+                <ContextMenuItem @click="$router.push('/swiper-slider')"> Swiper Slider </ContextMenuItem>
                 <ContextMenuItem @click="closeMenu"> Action 4 {{ contextData }} </ContextMenuItem>
                 <ContextMenuItem @click="closeMenu"> Cancel </ContextMenuItem>
             </template>
         </ContextMenu>
     </div>
 </template>
+<style scoped>
+    @media (min-width: 1024px) {
+        #homeView {
+            display: flex;
+            place-items: center;
+        }
+
+        #homeView {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            padding: 0 2rem;
+        }
+    }
+    header {
+        line-height: 1.5;
+        max-height: 100vh;
+    }
+
+    .logo {
+        display: block;
+        margin: 0 auto 2rem;
+    }
+
+    nav {
+        width: 100%;
+        font-size: 12px;
+        text-align: center;
+    }
+
+    nav a.router-link-exact-active {
+        color: var(--color-text);
+    }
+
+    nav a.router-link-exact-active:hover {
+        background-color: transparent;
+    }
+
+    nav a {
+        display: inline-block;
+        padding: 0 1rem;
+        border-left: 1px solid var(--color-border);
+    }
+
+    nav a:first-of-type {
+        border: 0;
+    }
+
+    @media (min-width: 1024px) {
+        header {
+            display: flex;
+            place-items: center;
+            padding-right: calc(var(--section-gap) / 2);
+        }
+
+        .logo {
+            margin: 0 2rem 0 0;
+        }
+
+        header .wrapper {
+            display: flex;
+            place-items: flex-start;
+            flex-wrap: wrap;
+        }
+
+        nav {
+            text-align: left;
+            margin-left: -1rem;
+            font-size: 1rem;
+        }
+    }
+</style>
